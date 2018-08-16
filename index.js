5d4657b85f1f5a477ff5e8d3b8562065b03cae67
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 21:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ class Route {
   };
 
   blocksTravelled(){
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     const streets = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
-    const avenues = 4;
+    const avenues = Math.abs(eastWest.findIndex(this.endingLocation.horizontal) - eastWest.findIndex(this.beginningLocation.horizontal));
     return streets + avenues;
   };
 
