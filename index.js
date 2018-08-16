49155d22855ca1aadca11c6da59960f86a959883
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 20:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ class Driver {
 
 class Route {
   constructor(beginningLocation, endingLocation) {
-    this.beginningLocation = beginningLocation;
+    
+    this.beginningLocation = {horizontal: };
     this.endingLocation = endingLocation;
   }
 }
