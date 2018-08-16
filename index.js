928c02700f205f946bcbe423963e7ee70a3695cc
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 20:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ class Driver {
   };
 
   yearsExperienceFromBeginningOf(year) {
-    return new Date(year, 0, 0) - this.startDate;
+    return (new Date(year, 0, 0) - this.startDate).getYear();
   };
 };
